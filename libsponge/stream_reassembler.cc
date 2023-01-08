8fa2e7888c329b00ffd2a6d27ba2289bd2c01037
dongliushi <2445264666@qq.com>
2023-01-08 15:07:05 +0800

    complete lab1

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -18,9 +18,31 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    // lab1文档中所提到的三个坐标，其中first_unread未被用到
+    // 每一个部分都是 [ , ) 左闭右开
+    size_t first_unassembled = _output.bytes_written();
+    size_t first_unacceptable = _output.bytes_read() + _capacity;
+    size_t data_len = data.size();
+    if (not _eof && eof) {
+        _eof = eof;
+        _eof_index = index + data_len;
+    }
+    // 数据正常到来
+    if (index <= first_unassembled && index + data_len > first_unassembled) {
+        // 实际上在这里如果到来的字节数大于capacity，那么对应的处理会将超出的字节舍去，再写入管道
+        // 而不是直接舍弃整个数据(说实话，我感觉应该全部舍弃的，但是全部舍弃过不了测试)
+        _output.write(data.substr(first_unassembled - index, first_unacceptable - first_unassembled));
+    } else if (index > first_unassembled) {  // 数据乱序到来
+        // 截取数据，超过缓冲区舍弃（但是为啥不是全部舍弃呢，晕晕）
+        _buffer.push_string(data.substr(0, first_unacceptable - index), index);
+    }
+    _buffer.reassemble(_output); // 重组缓冲区里信息
+    if (_eof && _output.bytes_written() == _eof_index) {
+        _buffer.clear();
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _buffer.nbytes(); }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _buffer.empty() && _output.buffer_empty(); }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -4,6 +4,9 @@
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <list>
+#include <memory>
+#include <queue>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
@@ -11,6 +14,74 @@
 class StreamReassembler {
   private:
     // Your code here -- add private members as necessary.
+    class ByteNodeList {  // 存储需要重组的字节信息
+        class ByteNode {  // 标识字节的index和len的结构
+          public:
+            std::shared_ptr<std::string> _data_ptr;
+            size_t _index;
+            size_t _len;
+            ByteNode(const std::string &data, const size_t index)
+                : _data_ptr(std::make_shared<std::string>(data)), _index(index), _len(data.size()) {}
+        };
+        auto find_first_insert(size_t index) {  // 找到第一个最前的可插入的节点
+            for (auto iter = _storage.begin(); iter != _storage.end(); ++iter) {
+                if (index < iter->_index + iter->_len) {
+                    return iter;
+                }
+            }
+            return _storage.end();
+        }
+        std::list<ByteNode> _storage = {};  // 存储节点的list
+
+      public:
+        size_t nbytes() const {  // 返回所存节点的字节总数
+            size_t ret = 0;
+            for (auto &node : _storage) {
+                ret += node._len;
+            }
+            return ret;
+        }
+        void reassemble(ByteStream &output) {  // 将能重组的字节输入到管道中
+            for (auto iter = _storage.begin(); iter != _storage.end();) {
+                if (iter->_index <= output.bytes_written()) {
+                    if (iter->_index + iter->_len > output.bytes_written()) {  // 这里判断插入是否有效
+                        output.write(iter->_data_ptr->substr(output.bytes_written() - iter->_index));
+                    }
+                    _storage.erase(iter++);
+                } else
+                    break;
+            }
+        }
+        bool empty() const { return _storage.empty(); }
+        void clear() { _storage.clear(); }
+        void push_string(const std::string &data, size_t index) {
+            size_t data_len = data.size();
+            auto iter = find_first_insert(index);
+            size_t end = index + data_len;
+            if (iter != _storage.end()) {                    // 判断是否有找到能插入的节点
+                if (index <= iter->_index) {                 // 插入数据的首字节在被插入节点的前面
+                    if (end >= iter->_index + iter->_len) {  // 情况1： 节点被覆盖，将该节点去除，重新插入
+                        _storage.erase(iter);
+                        push_string(data, index);
+                    } else if (end <= iter->_index) {  // 情况2： 被插入节点和插入数据无接触，直接插入
+                        _storage.insert(iter, ByteNode(data, index));
+                    } else if (end > iter->_index) {  // 情况3： 和被插入节点有交集，分割数据后重新插入
+                        push_string(data.substr(0, iter->_index - index), index);
+                    }
+                } else {  // 插入数据首字节在被插入节点里面
+                    if (end >
+                        iter->_index + iter->_len) {  // 插入数据长度需要大于被插入节点，否则直接抛弃(因为被覆盖了)
+                        push_string(data.substr(iter->_index + iter->_len - index), iter->_index + iter->_len);
+                    }
+                }
+            } else {  // 没找到，插入最后面
+                _storage.insert(iter, ByteNode(data, index));
+            }
+        }
+    };
+    ByteNodeList _buffer = {};
+    bool _eof = false;
+    size_t _eof_index = 0;
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
