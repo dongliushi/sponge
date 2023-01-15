b3fa222bcb2df0118809200dcbf370cf6f1416d6
dongliushi <2445264666@qq.com>
2023-01-15 14:12:04 +0800

    index < first_unacceptable to avoid index too big

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -32,7 +32,7 @@ void StreamReassembler::push_substring(const string &data, const size_t index, c
         // 实际上在这里如果到来的字节数大于capacity，那么对应的处理会将超出的字节舍去，再写入管道
         // 而不是直接舍弃整个数据(说实话，我感觉应该全部舍弃的，但是全部舍弃过不了测试)
         _output.write(data.substr(first_unassembled - index, first_unacceptable - first_unassembled));
-    } else if (index > first_unassembled) {  // 数据乱序到来
+    } else if (index > first_unassembled && index < first_unacceptable) {  // 数据乱序到来
         // 截取数据，超过缓冲区舍弃（但是为啥不是全部舍弃呢，晕晕）
         _buffer.push_string(data.substr(0, first_unacceptable - index), index);
     }
